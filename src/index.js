5461a6fc3729e711eee4279051d39a2d27acb872
Daniel <danielmtzlpz95@gmail.com>
2020-07-10 14:39:42 -0500

    Sexto guardado

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import { Todo, TodoList } from "./classes";
 // import { Todo } from "./classes/todo.class.js";
 // import { TodoList } from "./classes/todo-list.class.js";
 export const todoList = new TodoList();
-const tarea = new Todo("Aprender JavaScript");
-todoList.nuevoTodo(tarea);
-console.log(todoList);
-crearTodoHtml(tarea);
+// const tarea = new Todo("Aprender JavaScript");
+// todoList.nuevoTodo(tarea);
+// console.log(todoList);
+// crearTodoHtml(tarea);
+// Manera 1 de hacerlo 
+todoList.todos.forEach((todo) => crearTodoHtml(todo));
+// Manera dos de hacerlo
+// todoList.todos.forEach(crearTodoHtml);
+
